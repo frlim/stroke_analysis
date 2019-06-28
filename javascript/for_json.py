b463dc20a48899ec90afa285e889c4f5e28b277d
Truong <hqt2102@cumc.columbia.edu>
2019-06-28 11:15:17 -0400

    Reconfig to privatize api key

--- javascript/for_json.py
+++ javascript/for_json.py
@@ -4,26 +4,30 @@ import random
 import xlwings as xw
 import json
 
+'''Required input to run'''
+
 file_path = Path('Z:\\stroke_data\\')
 stroke_location_path = Path('C:\\Users\\hqt2102\\OneDrive - cumc.columbia.edu\\Stroke\\stroke_locations')
 out_path = Path.cwd()/'javascript/data_js'
 stroke_path = Path('C:\\Users\\hqt2102\\OneDrive - cumc.columbia.edu\\Stroke\\patrick_stroke')
-
+# generate random color or use old color
+generate_color = False
 
 MA_hospitals_latlong= pd.read_csv(file_path/'inner_join_siteID.csv',sep='|')
 # MA_hospitals_latlong.to_json(file_path/'MA_hopsitals_latlong.json',orient='records')
 
-points = pd.read_csv(stroke_location_path/'data/points/MA_n=100.csv')
-points.index.name='ID'
-points.reset_index(inplace=True)
-points_color = pd.read_csv(stroke_location_path/'output/maps/sexmale_age70.0_race8.0_symptom50.0_points_colors.csv')
-points_color.drop('ID',axis=1,inplace=True)
-points_w_color = points.merge(points_color,on=['Latitude','Longitude'])
-# points_w_color.to_json(out_path/'MA_n=100_points.js',orient='records')
+if generate_color:
+    points = pd.read_csv(stroke_location_path/'data/points/MA_n=100.csv')
+    points.index.name='ID'
+    points.reset_index(inplace=True)
+else:
+    # points_color = pd.read_csv(stroke_location_path/'output/maps/sexmale_age70.0_race8.0_symptom50.0_points_colors.csv')
+    points = pd.read_json(out_path/'MA_n=100_points.json')
+
 
 # get output of stroke_model
 output_path = stroke_path/'output'
-out_fname = 'times=MA_n=100_hospitals=MA_n=100_random_male_RACE_2.csv'
+out_fname = 'times=MA_n=100_hospitals=MA_n=100_random_male_RACE_9_s=20000.csv'
 model_output = pd.read_csv(output_path/out_fname)
 # group model output by patient profiles
 output_groups = model_output.groupby(['Sex','Age','Symptoms','RACE'])
@@ -39,16 +43,31 @@ centers = MA_hospitals_latlong.merge(hosp_key,on='AHA_ID',how='left',indicator=T
 centers_for_json=centers[['HOSP_KEY','Latitude','Longitude','CenterType','transfer_time','destinationID']]
 # centers_for_json.to_json(out_path/'MA_n=100_centers.js',orient='records')
 
-points= points_w_color
+def complement(r, g, b):
+    def hilo(a, b, c):
+        if c < b: b, c = c, b
+        if b < a: a, b = b, a
+        if c < b: b, c = c, b
+        return a + c
+    k = hilo(r, g, b)
+    return tuple(k - u for u in (r, g, b))
+
+
 out_list=[]
-generate_color = False
 
 for p_idx,row in points.iterrows():
     if generate_color:
-        color = "#"+"%06x" % random.randint(0, 0xFFFFFF)
+        # color = "#"+"%06x" % random.randint(0, 0xFFFFFF)
+        ra = lambda: random.randint(0,255)
+        r,g,b = ra(),ra(),ra()
+        cr,cg,cb = complement(r,g,b)
+        color = '#%02X%02X%02X'%(r,g,b)
+        complement_color = '#%02X%02X%02X'%(cr,cg,cb)
         points.loc[p_idx,'GMapColor'] = color # save color for later replotting
+        points.loc[p_idx,'GMapComColor'] = complement_color # save color for later replotting
     else:
         color = points.loc[p_idx,'GMapColor']
+        complement_color = points.loc[p_idx,'GMapComColor']
     p_lat,p_long = row.Latitude,row.Longitude
     print(f'Patient Coordinates {p_lat,p_long}')
     p_id = row.ID
@@ -61,16 +80,21 @@ for p_idx,row in points.iterrows():
         mask = centers['HOSP_KEY']==c_id
         c_lat,c_long = centers.loc[mask,'Latitude'].iloc[0],centers.loc[mask,'Longitude'].iloc[0]
         # print(f'Center Coordinates:{c_lat,c_long} Count:{weight}')
-        a_center = {"Center":c_id,"Weight":weight}
+        a_center = {"Center":int(c_id),"Weight":weight}
         centers_at_point.append(a_center)
-    out_list.append({"Point":{"Location":row["ID"],"Centers":centers_at_point,"Color":color}})
+    best_center = int(center_weights.idxmax())
+    out_list.append({"Point":{"Location":row["ID"],"Centers":centers_at_point,"Color":color,
+                    "ComColor":complement_color,
+                    "BestCenter":best_center}})
+
+if generate_color: points.to_json(out_path/'MA_n=100_points.json',orient='records')
 
 
 json_str=json.dumps(out_list)
-file = open(out_path/'MA_n=100_lines_RACE_2.js','w+')
-file.write('createLines(')
+file = open(out_path/'MA_n=100_lines_RACE_9_s=20000.json','w+')
+# file.write('createLines(')
 file.write(json_str)
-file.write(');\n')
-file.write('highLightLinesPoints();\n')
-file.write('highLightLinesCenters();')
+# file.write(');\n')
+# file.write('highLightLinesPoints();\n')
+# file.write('highLightLinesCenters();')
 file.close()
--- javascript/js/gmaps.js
+++ javascript/js/gmaps.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var map;
 var centersList;
 var pointsList;
@@ -7,7 +9,6 @@ const FULLOPACITY = 0.8;
 
 var pointIcons = ["https://img.icons8.com/ios/20/000000/secured-by-alarm-system.png",
 "https://img.icons8.com/ios/20/000000/secured-by-alarm-system-filled.png"];
-
 
 var blue_icon = "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=|7faeff";
 var red_icon = "https://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=|ffb3ad";
@@ -22,10 +23,10 @@ function getCenterIconByType(centerType){
   }
 }
 
-dataDIR='./data_js/';
+var dataDIR='./data_js/';
 var centersDIR = dataDIR + 'MA_n=100_centers.json';
 var pointsDIR = dataDIR + 'MA_n=100_points.json';
-var linesDIR = dataDIR + 'MA_n=100_lines_RACE_0.json';
+var linesDIR = dataDIR + 'MA_n=100_lines_RACE_0_s=20000.json';
 
 
 function initialize() {
@@ -54,67 +55,12 @@ function initialize() {
 
 function responseRACE(e){
   let raceNum = e.code[e.code.length-1];
-  linesDIR = dataDIR + "MA_n=100_lines_RACE_"+raceNum+".json";
+  linesDIR = dataDIR + "MA_n=100_lines_RACE_"+raceNum+"_s=20000.json";
   console.log(linesDIR);
   fetch(linesDIR).then(res => res.json()).then(function(data) {
     updateLines(data);});
   // only need to add listeners once
   // no need to call highLightLines() again (cause memory leaks)
-}
-
-
-function updateLines(results){
-  // remove old lines if ID is not in here
-  for (var i = 0; i < results.length; i++) {
-    var pointID = results[i].Point.Location;
-    var centerIDsList = [];
-    var preList = new Uint32Array(Object.keys(pointsList[pointID].lines));
-    console.log("old plotted list: "+preList);
-    // console.log(preList);
-    for (var c=0; c < results[i].Point.Centers.length;c++){
-      var centerID = results[i].Point.Centers[c].Center;
-      var weight = results[i].Point.Centers[c].Weight;
-      if (preList.includes(centerID)){
-        console.log("Change line point:"+pointID+" center:"+centerID+" weight to "+weight)
-        pointsList[pointID].lines[centerID].setWeight(weight);
-      }
-      else{
-        // create new obj
-        console.log("Create new line point:"+pointID+" center:"+centerID+" weight of "+weight)
-        var color = results[i].Point.Color;
-        var pLatLng = getPointCoordinates(pointID);
-        var cLatLng = getCenterCoordinates(centerID);
-        var PolylineCoordinates = [pLatLng,cLatLng];
-        var path = new google.maps.Polyline({
-          clickable: true,
-          geodesic: true,
-          path: PolylineCoordinates,
-          strokeColor: color,
-          strokeOpacity: BGOPACITY,
-          strokeWeight: Math.round(weight*20),
-          map:map
-        });
-        var lineObj = new Line(path,centerID,pointID);
-        registerCenter(centerID,lineObj);
-        registerPoint(pointID,lineObj);
-        // linesList.push(lineObj);
-      }
-      centerIDsList.push(centerID);
-    }
-    // find which line not in updated list of points\
-    console.log("center id list: "+centerIDsList);
-    console.log("includes test: "+centerIDsList);
-    centerIDsList = new Uint32Array(centerIDsList);
-    for (var c=0;c<preList.length;c++){
-      var centerID = preList[c];
-      if (!centerIDsList.includes(centerID)){
-        console.log("Delete line point:"+pointID+" center:"+centerID)
-        pointsList[pointID].lines[centerID].path.setMap(null);
-        delete centersList[centerID].lines[pointID];
-        delete pointsList[pointID].lines[centerID];
-      }
-    }
-  }
 }
 
 function createMarkers(results){
@@ -127,7 +73,7 @@ function createMarkers(results){
       var hKey = results[i].HOSP_KEY;
       var name = "HOSP_KEY " + hKey.toString();
       var type = results[i].CenterType;
-      icons= getCenterIconByType(results[i].CenterType);
+      var icons= getCenterIconByType(results[i].CenterType);
       var marker = new google.maps.Marker({
         title: name.toString(),
         position: latLng,
@@ -159,6 +105,7 @@ function createMarkersForPoints(results){
     }
 }
 
+
 function createLines(results){
     // Loop through the results array and place a marker for each
   // set of coordinates.
@@ -166,6 +113,8 @@ function createLines(results){
       var pointID = results[i].Point.Location;
       var pLatLng = getPointCoordinates(pointID);
       var color = results[i].Point.Color;
+      var comColor = results[i].Point.ComColor;
+      var bestCenterID = results[i].Point.BestCenter;
       for (var c=0; c < results[i].Point.Centers.length;c++){
           var centerID = results[i].Point.Centers[c].Center;
           var weight = results[i].Point.Centers[c].Weight;
@@ -180,28 +129,122 @@ function createLines(results){
           strokeWeight: Math.round(weight*20),
           map:map
           });
+          // console.log(comColor)
           var lineObj = new Line(path,centerID,pointID);
+          if (centerID==bestCenterID){
+            var lineSymbol = createDash(comColor);
+            lineObj.setSymbol(lineSymbol);}
           registerCenter(centerID,lineObj);
           registerPoint(pointID,lineObj);
-          // linesList.push(lineObj);
       }
     }
 }
 
-function clearLinesFromMap(lineLists){
-  for (var i=0;i<lineLists.length;i++){
-    linesList[i].path.setMap(null);
-  }
+
+function createDash(comColor){
+    var comColor = typeof comColor !== 'undefined' ? comColor : "#000000";
+    // Define a symbol using SVG path notation, with an opacity of 1.
+    var lineSymbol = {
+      path: 'M 0,-1 0,1',
+      strokeOpacity: 1,
+      strokeColor: comColor,
+      scale: 3,
+    };
+    return lineSymbol;
 }
 
-function clearLinesReferences(){
-  linesList=[];
-  // reset these referrences
-  for (const key in pointsList){
-    pointsList[key].removeLines();
+function updateLines(results){
+  // remove old lines if ID is not in here
+  for (var i = 0; i < results.length; i++) {
+    var pointID = results[i].Point.Location;
+    var centerIDsList = [];
+    var preList = new Uint32Array(Object.keys(pointsList[pointID].lines));
+    console.log("old plotted list: "+preList);
+    // console.log(preList);
+    var comColor = results[i].Point.ComColor;
+    var bestCenterID = results[i].Point.BestCenter;
+    console.log('best center is '+bestCenterID)
+    for (var c=0; c < results[i].Point.Centers.length;c++){
+      var centerID = results[i].Point.Centers[c].Center;
+      var weight = results[i].Point.Centers[c].Weight;
+      if (preList.includes(centerID)){
+        console.log("Change line point:"+pointID+" center:"+centerID+" weight to "+weight)
+        pointsList[pointID].lines[centerID].setWeight(weight);
+        if (centerID != bestCenterID && pointsList[pointID].lines[centerID].bestLineStatus){
+          console.log("previously best line but will be unset");
+          pointsList[pointID].lines[centerID].unsetSymbol();
+        }
+        if (centerID == bestCenterID && !pointsList[pointID].lines[centerID].bestLineStatus){
+          var lineSymbol=createDash(comColor);
+          console.log("set to be new best line");
+          pointsList[pointID].lines[centerID].setSymbol(lineSymbol);
+        }
+      }
+      else{
+        // create new obj
+        console.log("Create new line point:"+pointID+" center:"+centerID+" weight of "+weight)
+        var color = results[i].Point.Color;
+        var pLatLng = getPointCoordinates(pointID);
+        var cLatLng = getCenterCoordinates(centerID);
+        var PolylineCoordinates = [pLatLng,cLatLng];
+        var path = new google.maps.Polyline({
+          clickable: true,
+          geodesic: true,
+          path: PolylineCoordinates,
+          strokeColor: color,
+          strokeOpacity: BGOPACITY,
+          strokeWeight: Math.round(weight*20),
+          map:map
+        });
+        var lineObj = new Line(path,centerID,pointID);
+        if (centerID == bestCenterID){
+          console.log("set to be new best line");
+          lineSymbol=createDash(comColor);
+          lineObj.setSymbol(lineSymbol);}
+        registerCenter(centerID,lineObj);
+        registerPoint(pointID,lineObj);
+        // linesList.push(lineObj);
+      }
+      centerIDsList.push(centerID);
+    }
+    console.log("actual centerID for this race score: "+centerIDsList);
+    // find which line not in updated list of points\
+    centerIDsList = new Uint32Array(centerIDsList);
+    for (var c=0;c<preList.length;c++){
+      var centerID = preList[c];
+      if (!centerIDsList.includes(centerID)){
+        console.log("Delete line point:"+pointID+" center:"+centerID)
+        pointsList[pointID].unmountLineFromMap(centerID);
+        pointsList[pointID].deleteLineObj(centerID);
+        centersList[centerID].deleteLineObj(pointID);
+      }
+    }
   }
-  for (const key in centersList){
-    centersList[key].removeLines();
+  updateHighLight();
+}
+
+function updateHighLight(){
+  for (var pointID in pointsList){
+    var pointObj = pointsList[pointID];
+    if (pointObj.highlightStatus){
+      // console.log('rehighlight after update point');
+      pointObj.highlightOn();}
+    if (pointObj.blockHoverStatus){
+      pointObj.blockHover();}
+    if (!pointObj.highlightStatus){pointObj.highlightOff();}
+    if (!pointObj.blockHoverStatus){
+      pointObj.unblockHover();}
+  }
+  for (var centerID in centersList){
+    var centerObj = centersList[centerID];
+    if (centerObj.highlightStatus){
+      // console.log('rehighlight after update center');
+      centerObj.highlightOn();}
+    if (centerObj.blockHoverStatus){
+      centerObj.blockHover();}
+    if (!centerObj.highlightStatus){centerObj.highlightOff();}
+    if (!centerObj.blockHoverStatus){
+      centerObj.unblockHover();}
   }
 }
 
@@ -262,7 +305,7 @@ function highLightLinesCenters(){
 function highLightLinesPoints(){
   for (const key in pointsList){
     pointsList[key]["marker"].addListener("click", function(){
-      console.log("clicked on Center "+pointsList[key].ID);
+      console.log("clicked on Point "+pointsList[key].ID);
       if (!pointsList[key].blockHoverStatus){ // to prevent multi click error
         // if only not block yet, then highlight and block it
         pointsList[key].highlightOn();
@@ -271,7 +314,7 @@ function highLightLinesPoints(){
       }
      );
     pointsList[key]["marker"].addListener("rightclick", function(){
-      console.log("right-clicked on Center"+pointsList[key].ID);
+      console.log("right-clicked on Point "+pointsList[key].ID);
       if (pointsList[key].blockHoverStatus){
         pointsList[key].unblockHover();
         pointsList[key].highlightOff();
@@ -279,13 +322,13 @@ function highLightLinesPoints(){
       }
     );
     pointsList[key]["marker"].addListener("mouseover", function(){
-      console.log("mouseover on Center "+pointsList[key].ID);
+      console.log("mouseover on Point "+pointsList[key].ID);
       if (!pointsList[key].blockHoverStatus){
           pointsList[key].highlightOn();
         }
     });
     pointsList[key]["marker"].addListener("mouseout", function(){
-      console.log("mouseout on Center "+pointsList[key].ID);
+      console.log("mouseout on Point "+pointsList[key].ID);
       if (!pointsList[key].blockHoverStatus){
           pointsList[key].highlightOff();
         }
@@ -299,7 +342,7 @@ class Center {
     this.type=type;
     this.ID = ID;
     this.lines={};
-    this.highlight=false;
+    this.highlightStatus=false;
     this.blockHoverStatus=false;
   }
   addLine(lineObj){
@@ -309,18 +352,17 @@ class Center {
     this.lines={};
   }
   highlightOn(){
-    this.highlight=true;
+    this.highlightStatus=true;
     // changeicon
     this.marker.setIcon(getCenterIconByType(this.type)[1]);
     // highlight lines;
     for (const l in this.lines){
       var line = this.lines[l];
-      // if not on, set on
-      if (!line.highlightStatus){ line.highlightOn();}
+      line.highlightOn();
     }
   }
   highlightOff(){
-    this.highlight=false;
+    this.highlightStatus=false;
     // changeicon
     this.marker.setIcon(getCenterIconByType(this.type)[0]);
     // highlight lines;
@@ -333,7 +375,10 @@ class Center {
   blockHover(){
     for (const l in this.lines){
       var line = this.lines[l];
-      line.addCenterBlock(this.ID);
+      // only add if not already in block list
+      if (!line.checkInCenterBlock(this.ID)){
+        line.addCenterBlock(this.ID);
+      }
     }
     this.blockHoverStatus=true;
   }
@@ -344,9 +389,11 @@ class Center {
     }
     this.blockHoverStatus=false;
   }
-  get linesStuff(){
-    console.log('get lineStuff')
-    return this.lines;
+  unmountLineFromMap(pointID){
+    this.lines[pointID].unmountFromMap();
+  }
+  deleteLineObj(pointID){
+    delete this.lines[pointID];
   }
 }
 
@@ -355,7 +402,7 @@ class Point {
     this.marker=marker;
     this.ID = ID;
     this.lines={};
-    this.highlight=false;
+    this.highlightStatus=false;
     this.blockHoverStatus=false;
   }
   addLine(lineObj){
@@ -365,44 +412,52 @@ class Point {
     this.lines={};
   }
   highlightOn(){
-    this.highlight=true;
+    this.highlightStatus=true;
     // changeicon
     this.marker.setIcon(pointIcons[1]);
     // highlight lines;
     for (const l in this.lines){
       var line = this.lines[l];
-      // if not on, set on
-      if (!line.highlight){ line.highlightOn();}
+      // console.log('in point:'+this.ID + ' might highlight line center:'+l+' its highlightStatus is '+line.highlightStatus);
+      line.highlightOn();
     }
   }
   highlightOff(){
-    this.highlight=false;
+    this.highlightStatus=false;
     // changeicon
     this.marker.setIcon(pointIcons[0]);
     // highlight lines;
     for (const l in this.lines){
       var line = this.lines[l];
-      // if there are no block then set highlight off
-      if (!line.anyBlock){ line.highlightOff();}
+      // console.log('in point:'+this.ID + ' might OFFhighlight line center:'+l+' its highlightStatus is '+line.highlightStatus);
+      // if there are no block and currently on then set highlight off
+      if (!line.anyBlock && line.highlightStatus){
+        // console.log('highlight off called');
+        line.highlightOff();}
     }
   }
   blockHover(){
     for (const l in this.lines){
       var line = this.lines[l];
-      line.addCenterBlock(this.ID);
+      // only add if not already in block list
+      if (!line.checkInPointBlock(this.ID)){
+        line.addPointBlock(this.ID);
+      }
     }
     this.blockHoverStatus=true;
   }
   unblockHover(){
     for (const l in this.lines){
       var line = this.lines[l];
-      line.removeCenterBlock(this.ID);
+      line.removePointBlock(this.ID);
     }
     this.blockHoverStatus=false;
   }
-  get linesStuff(){
-    console.log('get lineStuff')
-    return this.lines;
+  unmountLineFromMap(centerID){
+    this.lines[centerID].unmountFromMap();
+  }
+  deleteLineObj(centerID){
+    delete this.lines[centerID];
   }
 }
 
@@ -414,6 +469,18 @@ class Line {
     this.centerBlockHover = [];
     this.pointBlockHover = [];
     this.highlightStatus=false;
+    this.bestLineStatus = false; //best line will get a dash on top
+  }
+  setSymbol(lineSymbol){
+    this.bestLineStatus=true;
+    this.lineSymbol=lineSymbol;
+  }
+  unsetSymbol(){
+    this.bestLineStatus=false;
+    delete this.lineSymbol;
+    this.path.setOptions({
+      "icons":[]
+    });
   }
   registerCenter(center){
     this.center=center;
@@ -430,18 +497,42 @@ class Line {
   removePointBlock(pointID){
     this.pointBlockHover.splice(this.pointBlockHover.indexOf(pointID),1);
   }
+  checkInPointBlock(pointID){
+    return this.pointBlockHover.indexOf(pointID) > -1;
+  }
+  checkInCenterBlock(centerID){
+    return this.centerBlockHover.indexOf(centerID) > -1;
+  }
   removeCenterBlock(centerID){
     this.centerBlockHover.splice(this.centerBlockHover.indexOf(centerID),1);
   }
   highlightOn(){
+    this.highlightStatus=true;
     this.path.setOptions({
       "strokeOpacity":FULLOPACITY
     });
+    if (this.bestLineStatus){
+      this.lineSymbol.strokeOpacity=1;
+      this.path.setOptions({"icons": [{
+              icon: this.lineSymbol,
+              offset: '0',
+              repeat: '10px'
+      }]});
+    }
   }
   highlightOff(){
+    this.highlightStatus=false;
     this.path.setOptions({
       "strokeOpacity":BGOPACITY
     });
+    if (this.bestLineStatus){
+      this.lineSymbol.strokeOpacity=0;
+      this.path.setOptions({"icons": [{
+              icon: this.lineSymbol,
+              offset: '0',
+              repeat: '10px'
+      }]});
+    }
   }
   get anyBlock(){
     return (this.pointBlockHover.length>0) || (this.centerBlockHover.length>0);
@@ -451,5 +542,8 @@ class Line {
     this.path.setOptions({
       "strokeWeight":strokeWeight
     });
+  }
+  unmountFromMap(){
+    this.path.setMap(null);
   }
 }
