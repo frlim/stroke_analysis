8c0c5f1ac2e23e3c7ff2c7a17b340c24278f0d32
ryanhantruong <hqt2102@cumc.columbia.edu>
2019-08-07 09:38:45 -0400

    change path for ted output

--- data_io.py
+++ data_io.py
@@ -4,18 +4,18 @@ import os
 if os.name == 'nt':
     DTN_PATH = Path('Z:\\stroke_data\\processed_data')
     MODELOUT_PATH = Path.cwd().parent / 'patrick_stroke' / 'output'
-    LOCAL_OUTPUT = Path('F:\\stroke_model_output\\output_073019')
+    LOCAL_OUTPUT = Path('F:\\stroke_model_output\\output_080619')
     MARKOV_ANALYSIS_OUTPUT = Path(
-        'F:\\stroke_model_output\\markov_describe_073019\\')
+        'F:\\stroke_model_output\\markov_describe_080619\\')
     BASIC_ANALYSIS_OUTPUT = Path(
-        'F:\\stroke_model_output\\results_analysis_073019\\')
-    GRAPH_OUTPUT = Path('F:\\stroke_model_output\\plots_073019\\')
+        'F:\\stroke_model_output\\results_analysis_080619\\')
+    GRAPH_OUTPUT = Path('F:\\stroke_model_output\\plots_080619\\')
 else:
     DTN_PATH = Path('~/deidentified_stroke_data')
     MODELOUT_PATH = Path.cwd().parent / 'patrick_stroke' / 'output'
-    LOCAL_OUTPUT = Path('/sda1/stroke_model_output/output_073019')
+    LOCAL_OUTPUT = Path('/sda1/stroke_model_output/output_080619')
     MARKOV_ANALYSIS_OUTPUT = Path(
-        '/sda1/stroke_model_output/markov_describe_073019')
+        '/sda1/stroke_model_output/markov_describe_080619')
     BASIC_ANALYSIS_OUTPUT = Path(
-        '/sda1/stroke_model_output/results_analysis_073019')
-    GRAPH_OUTPUT = Path('/sda1/stroke_model_output/plots_073019')
+        '/sda1/stroke_model_output/results_analysis_080619')
+    GRAPH_OUTPUT = Path('/sda1/stroke_model_output/plots_080619')
